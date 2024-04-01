53d7109155cdaeeca6cc086cfb9f3625a0dc0786
GakuruUri <urigakuru@icloud.com>
2024-04-01 14:44:43 +0300

    cs50p day 2

--- loops/hogwarts.py
+++ loops/hogwarts.py
@@ -36,9 +36,11 @@
 
 
 students = [
-    {"name": "Hermoine", "House": "Gryffindor", "patronus": "Otter"},
+    {"name": "Hermoine", "house": "Gryffindor", "patronus": "Otter"},
     {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
-    {"name": "Ron", "House": "Gryffindor", "patronus": "Jack Russel terrier"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russel terrier"},
     {"name": "Draco", "house": "Slytherin", "patronus": None},   
 ]
 
+for student in students:
+    print(student["name"], student["house"], student["patronus"], sep=", ")
