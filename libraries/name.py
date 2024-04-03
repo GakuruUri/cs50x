4d31fcfd0f2552346a6ca4591a661ba0c6dbc013
GakuruUri <urigakuru@icloud.com>
2024-04-03 22:23:50 +0300

    Cs50

---
+++ libraries/name.py
@@ -0,0 +1,17 @@
+# argument vector
+import sys
+
+# try:
+#     print("hello, my name is", sys.argv[1])
+# except IndexError:
+#     print("Too few arguments")
+
+
+if len(sys.argv) < 2:
+    print("Too few arguments")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+else:
+    print("Hello, my name is", sys.argv[1])
+
+
